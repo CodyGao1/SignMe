31af5a32598cdda6c3c591ba952b13a98ab37929
ILikeCheeseCow <105215146+ILikeCheeseCow@users.noreply.github.com>
2024-04-04 18:47:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,104 +8,124 @@ import { non_max_suppression } from "./utils/nonMaxSuppression";
 import "./style/App.css";
 
 function shortenedCol(arrayofarray, indexlist) {
-    return arrayofarray.map(array => indexlist.map(idx => array[idx]));
+  return arrayofarray.map(array => indexlist.map(idx => array[idx]));
 }
 
 function mapIdToLetter(id) {
-    if (id === 25) return ' '; // Map 25 to a blank space
-    return String.fromCharCode(65 + id); // Map 0-24 to A-Y
+  if (id === 25) return ' '; // Map 25 to a blank space
+  return String.fromCharCode(65 + id); // Map 0-24 to A-Y
+}
+
+function mostCommon(arr) {
+  const frequencyMap = {};
+  arr.forEach(item => {
+    frequencyMap[item] = (frequencyMap[item] || 0) + 1;
+  });
+
+  return Object.keys(frequencyMap).reduce((a, b) => frequencyMap[a] > frequencyMap[b] ? a : b);
 }
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-    const [outputText, setOutputText] = useState('');
-    const [intervalDuration, setIntervalDuration] = useState(2000); // State for slider
-    const videoRef = useRef(null);
-    const canvasRef = useRef(null);
-    const webcam = new Webcam();
-    const modelName = "ASL";
-    const threshold = 0.85;
+  const [loading, setLoading] = useState(true);
+  const [outputText, setOutputText] = useState('');
+  const [intervalDuration, setIntervalDuration] = useState(2000);
+  const [detectionBuffer, setDetectionBuffer] = useState([]);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const webcam = new Webcam();
+  const modelName = "ASL";
+  const threshold = 0.85;
 
-    const detectFrame = async (model) => {
-        const model_dim = [512, 512];
-        tf.engine().startScope();
-        const input = tf.tidy(() => {
-            const img = tf.image.resizeBilinear(tf.browser.fromPixels(videoRef.current), model_dim)
-                                .div(255.0)
-                                .transpose([2, 0, 1])
-                                .expandDims(0);
-            return img;
-        });
+  const detectFrame = async (model) => {
+    const model_dim = [512, 512];
+    tf.engine().startScope();
+    const input = tf.tidy(() => {
+      const img = tf.image
+                .resizeBilinear(tf.browser.fromPixels(videoRef.current), model_dim)
+                .div(255.0)
+                .transpose([2, 0, 1])
+                .expandDims(0);
+      return img;
+    });
 
-        const res = model.execute(input);
-        const predictions = res.arraySync();
+    const res = model.execute(input);
+    const predictions = res.arraySync();
 
-        var detections = non_max_suppression(predictions[0]);
-        const boxes = shortenedCol(detections, [0,1,2,3]);
-        const scores = shortenedCol(detections, [4]);
-        const class_detect = shortenedCol(detections, [5]);
+    var detections = non_max_suppression(predictions[0]);
+    const boxes = shortenedCol(detections, [0,1,2,3]);
+    const scores = shortenedCol(detections, [4]);
+    const class_detect = shortenedCol(detections, [5]);
 
-        if (class_detect.length > 0 && class_detect[0][0] !== 25) {
-            setOutputText(currentText => currentText + mapIdToLetter(class_detect[0][0]));
-        }
+    if (class_detect.length > 0) {
+        setDetectionBuffer(currentBuffer => [...currentBuffer, class_detect[0][0]]);
+    }
 
-        renderBoxes(canvasRef, threshold, boxes, scores, class_detect);
-        tf.dispose(res);
-        tf.dispose(input);
+    renderBoxes(canvasRef, threshold, boxes, scores, class_detect);
+    tf.dispose(res);
+    tf.dispose(input);
 
-        requestAnimationFrame(() => detectFrame(model));
-        tf.engine().endScope();
-    };
+    requestAnimationFrame(() => detectFrame(model));
+    tf.engine().endScope();
+  };
 
-    useEffect(() => {
-        tf.loadGraphModel(`${window.location.origin}/${modelName}_web_model/model.json`)
-          .then(model => {
-              setLoading(false);
-              webcam.open(videoRef, () => detectFrame(model));
-          })
-          .catch(error => {
-              console.error("Error loading model", error);
-              setLoading(false);
-          });
-    }, []);
+  useEffect(() => {
+    tf.loadGraphModel(`${window.location.origin}/${modelName}_web_model/model.json`)
+      .then(model => {
+        setLoading(false);
+        webcam.open(videoRef, () => detectFrame(model));
+      });
+  }, []);
 
-    const clearOutput = () => {
-        setOutputText('');
-    };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (detectionBuffer.length > 0) {
+        const mostCommonDetection = mostCommon(detectionBuffer);
+        const newLetter = mapIdToLetter(mostCommonDetection);
+        setOutputText(currentText => currentText + newLetter);
+        setDetectionBuffer([]);
+      }
+    }, intervalDuration);
+    return () => clearInterval(interval);
+  }, [intervalDuration, detectionBuffer]);
 
-    return (
-        <div className="App">
-            <h2>SignMe</h2>
-            {loading ? (
-                <Loader>Loading model...</Loader>
-            ) : (
-                <>
-                    <div className="content">
-                        <video autoPlay playsInline muted ref={videoRef} id="frame" />
-                        <canvas width={512} height={512} ref={canvasRef} />
-                    </div>
-                    
-                    <div className="output-area">
-                        {outputText}
-                    </div>
+  const clearOutput = () => {
+    setOutputText('');
+    setDetectionBuffer([]);
+  };
 
-                    <div className="slider-container">
-                        <input
-                            type="range"
-                            min="500"
-                            max="5000"
-                            value={intervalDuration}
-                            onChange={e => setIntervalDuration(Number(e.target.value))}
-                            className="slider"
-                        />
-                        <p>Interval: {intervalDuration} ms</p>
-                    </div>
-                    
-                    <button onClick={clearOutput} className="clear-button">Clear</button>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="App">
+      <h2>SignMe</h2>
+      {loading ? (
+        <Loader>Loading model...</Loader>
+      ) : (
+        <>
+          <div className="content">
+            <video autoPlay playsInline muted ref={videoRef} id="frame" />
+            <canvas width={512} height={512} ref={canvasRef} />
+          </div>
+          
+          <div className="output-area">
+            {outputText}
+          </div>
+
+          <div className="slider-container">
+            <input
+              type="range"
+              min="500"
+              max="5000"
+              value={intervalDuration}
+              onChange={(e) => setIntervalDuration(Number(e.target.value))}
+              className="slider"
+            />
+            <p>Interval: {intervalDuration} ms</p>
+          </div>
+
+          <button onClick={clearOutput} className="clear-button">Clear</button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
